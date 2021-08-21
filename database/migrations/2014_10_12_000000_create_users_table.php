f2d1e27a18553850650863ed650925fad00548d1
BolBol54 <zeka.bolbol@gmail.com>
2021-08-21 19:03:20 +0200

    npm and composer packages

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->boolean('isAdmin');
+            $table->string('status');
+            $table->integer('type');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
