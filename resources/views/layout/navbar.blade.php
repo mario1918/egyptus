7814c842be19b705f9c5f0ac1877d6b952daae82
mario1918 <43424896+mario1918@users.noreply.github.com>
2021-10-21 12:38:37 +0200

    another nav bar edits

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,14 +1,14 @@
 <!--start navbar -->
 
 <nav class=" second-nav navbar navbar-expand-lg navbar-mainbg">
-  <p class=" navbar-brand navbar-logo" style="margin-left: 8%;">Egyptus<p>
+  <p class=" navbar-brand navbar-logo" style="margin-left: 6%;"><a href="{{route('home')}}">Egyptus</a><p>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <i class="fa fa-bars text-white"></i>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-right: 8%;">
-      <ul class="navbar-nav ml-auto">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-right: 6%;">
+      <ul class="navbar-nav ml-auto mr-2">
           <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
 
           <!--
@@ -27,7 +27,7 @@
       <li class="nav-item pr-4">
         <a class="nav-link" href="#">About </a>
       </li>
-
+<!--
   @auth
     <li class="nav-item pr-4">
       <form class="form-inline">
@@ -44,9 +44,23 @@
       </li>
         @endauth
   </li>
+-->
 
      
       </ul>
+
+    
+
+      <form class="form-inline my-2 my-lg-0 ml-5">
+      @auth
+      <button type="button" class="btn my-2 my-sm-0 mr-1 px-3 account-btn">Log out</button>
+      @else
+        <button class="btn my-2 my-sm-0 mr-1 px-3 account-btn">Login</button>
+        <button class="btn my-2 my-sm-0 ml-1 px-3 account-btn">Sign up as a Guide</button>
+      @endauth
+      </form>
+
+      
       
   </div>
   
