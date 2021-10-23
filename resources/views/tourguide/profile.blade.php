f7bdc39a0887be0055ee8d1d26e965f988bbc563
mario1918 <43424896+mario1918@users.noreply.github.com>
2021-10-23 09:45:21 +0200

    added languages to tourguide profile

--- resources/views/tourguide/profile.blade.php
+++ resources/views/tourguide/profile.blade.php
@@ -72,6 +72,15 @@
                           <strong>${{$tourguide->priceRate}}</strong>/hour
                         </div>
                       </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <h6 class="mb-0">Languages</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                          {{$tourguide->languages}}
+                        </div>
+                      </div>
 
                  
                     </div>
